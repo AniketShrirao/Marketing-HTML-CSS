b82b43191c8701370bb7ab4d47100e0f35a00b31
saniketprdxn <saniketprdxn@github.com>
2020-03-12 15:06:27 +0530

    js  slider started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 /* Author: Aniket*/
 
+ /*=================================
+  Slider function starts here
+=================================*/
+// Slider Container
+var slider = document.querySelector(".slider");
+// Control Buttons
+var btns = document.querySelectorAll(".btn");
+// Slides/Images
+var slides = document.querySelectorAll(".slide figure");
+// Carousel Dots
+var dots = document.querySelectorAll('.dot');
 
 
 
+ /*=================================
+  Slider function ends here
+=================================*/
 
 
 
