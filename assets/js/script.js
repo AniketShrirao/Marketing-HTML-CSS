4ee4a7e23db5c19085dd0b6876bd38f8b2fd64d4
saniketprdxn <saniketprdxn@github.com>
2020-03-12 18:10:11 +0530

    js  Contact form validation started

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,107 +2,107 @@
  /*=================================
   Slider function starts here
 =================================*/
-// Slider Container
-var slider = document.querySelector(".slider");
-// Control Buttons
-var btns = document.querySelectorAll(".btn");
-// Slides/Images
-var slides = document.querySelectorAll(".slide figure");
-// Carousel Dots
-var dots = document.querySelectorAll('.dot');
+// // Slider Container
+// var slider = document.querySelector(".slider");
+// // Control Buttons
+// var btns = document.querySelectorAll(".btn");
+// // Slides/Images
+// var slides = document.querySelectorAll(".slide figure");
+// // Carousel Dots
+// var dots = document.querySelectorAll('.dot');
 
-// carousel utility function
-function SliderCarousel(slider,btns,slides,dots) {
-	var index = 1;
-	var dotIndex = 0;
-	var size = '100';
+// // carousel utility function
+// function SliderCarousel(slider,btns,slides,dots) {
+// 	var index = 1;
+// 	var dotIndex = 0;
+// 	var size = '100';
 
-	// to Animate the caousel
-	setInterval(ChangeDotOnBtn,4000);
+// 	// to Animate the caousel
+// 	setInterval(ChangeDotOnBtn,4000);
 
-// To transition the slides
-	function slide() {
-		slider.classList.remove('transition-inactive');		
-		slider.classList.add('transition-active');
-		update();
-	}
+// // To transition the slides
+// 	function slide() {
+// 		slider.classList.remove('transition-inactive');		
+// 		slider.classList.add('transition-active');
+// 		update();
+// 	}
 
-	// To translate the slides
-	function update() {
-		slider.style.transform = "translateX("+ (-size * index) +"%)";
-		// to make current Carousel dot active
-		dots.forEach(function(dot) {
-			dot.classList.remove('dot-active');
-		});
-		dots[dotIndex].classList.add('dot-active');
-	}
-	update();
+// 	// To translate the slides
+// 	function update() {
+// 		slider.style.transform = "translateX("+ (-size * index) +"%)";
+// 		// to make current Carousel dot active
+// 		dots.forEach(function(dot) {
+// 			dot.classList.remove('dot-active');
+// 		});
+// 		dots[dotIndex].classList.add('dot-active');
+// 	}
+// 	update();
 
-	// change the index of carousel dots based on current slide
-	function ChangeDotOnBtn() {
-		if(this.id === "prev") {
-			if(index <= 0) {
-				return;
-			}
-			index--;
-			if(dotIndex === 0){
-				dotIndex = 4;
-			}
-			else {
-				dotIndex--;
-			}
-		}
-		else {
-			if(index === slides.length-1 ) {
-				return;
-			}
-			index++;
-			if(dotIndex === 4) {
-				dotIndex = 0;
-			}
-			else {
-				dotIndex++;
-			}
-		}
-		slide();
-	}
+// 	// change the index of carousel dots based on current slide
+// 	function ChangeDotOnBtn() {
+// 		if(this.id === "prev") {
+// 			if(index <= 0) {
+// 				return;
+// 			}
+// 			index--;
+// 			if(dotIndex === 0){
+// 				dotIndex = 4;
+// 			}
+// 			else {
+// 				dotIndex--;
+// 			}
+// 		}
+// 		else {
+// 			if(index === slides.length-1 ) {
+// 				return;
+// 			}
+// 			index++;
+// 			if(dotIndex === 4) {
+// 				dotIndex = 0;
+// 			}
+// 			else {
+// 				dotIndex++;
+// 			}
+// 		}
+// 		slide();
+// 	}
 
-	// get the dots current value from data-attribute
-	function dotChange() {
-		let i = Number(this.getAttribute('dot-index'));
-		dotIndex = i;
-		index = i + 1;
-		slide();
-	}
+// 	// get the dots current value from data-attribute
+// 	function dotChange() {
+// 		let i = Number(this.getAttribute('dot-index'));
+// 		dotIndex = i;
+// 		index = i + 1;
+// 		slide();
+// 	}
 
-	// cloning of first and last slide to give slider effect at the end of every transition
-	slider.addEventListener('transitionend', function() {
-		if(slides[index].parentNode.id === "first") {
-			slider.classList.add('transition-inactive');		
-			slider.classList.remove('transition-active');
-			index = slides.length - 2;
-			slider.style.transform = "translateX("+ (-size * index) +"%)";
-		}
-		else if(slides[index].parentNode.id === "last") {
-			slider.classList.add('transition-inactive');		
-			slider.classList.remove('transition-active');
-			index = 1;
-			slider.style.transform = "translateX("+ (-size * index) +"%)";
-		}
-	})
-	// Events assigned to the Control buttons
-	btns.forEach( function(btn) {
-		btn.addEventListener('click', ChangeDotOnBtn);
-		clearInterval(ChangeDotOnBtn);
-	});
-	// Events assigned to the Carousel dots
-	dots.forEach(function(dot) {
-		dot.addEventListener('click', dotChange);
-	});
-}
+// 	// cloning of first and last slide to give slider effect at the end of every transition
+// 	slider.addEventListener('transitionend', function() {
+// 		if(slides[index].parentNode.id === "first") {
+// 			slider.classList.add('transition-inactive');		
+// 			slider.classList.remove('transition-active');
+// 			index = slides.length - 2;
+// 			slider.style.transform = "translateX("+ (-size * index) +"%)";
+// 		}
+// 		else if(slides[index].parentNode.id === "last") {
+// 			slider.classList.add('transition-inactive');		
+// 			slider.classList.remove('transition-active');
+// 			index = 1;
+// 			slider.style.transform = "translateX("+ (-size * index) +"%)";
+// 		}
+// 	})
+// 	// Events assigned to the Control buttons
+// 	btns.forEach( function(btn) {
+// 		btn.addEventListener('click', ChangeDotOnBtn);
+// 		clearInterval(ChangeDotOnBtn);
+// 	});
+// 	// Events assigned to the Carousel dots
+// 	dots.forEach(function(dot) {
+// 		dot.addEventListener('click', dotChange);
+// 	});
+// }
 
-// carousel utility function called
-SliderCarousel(slider,btns,slides,dots);
+// // carousel utility function called
+// SliderCarousel(slider,btns,slides,dots);
 
  /*=================================
   Slider function ends here
@@ -227,3 +227,32 @@ LightBox(html,ownersLightBox,Lightbox,contentInsideLightbox,close);
  /*=================================
   owners-lightBox function ends here
 =================================*/
+
+ /*=================================
+  ContactUs form function start here
+=================================*/
+
+// Selecting Every Input field
+var inputs = [
+  firstName = document.querySelector('#Contact-form-name'),
+  email = document.querySelector('#Contact-form-email'),
+  subject = document.querySelector('#Contact-form-subject'),
+  message = document.querySelector('#Contact-form-message'),
+];
+// Retrieving Values of every input field
+var values = {
+  fname: firstName.value,
+  emailId: email.value,
+  subject: subject.value,
+  message: subject.value
+};
+// Regex for every input fields
+var Regex = [
+  firstNameRegex = /([a-zA-Z]){1,20}$/,
+  emailRegex = /^([0-9a-zA-Z\_\.\-]+)@([0-9a-zA-Z\_\.\-]+)\.([a-zA-Z]+)$/,
+  subjectRegex = /([a-zA-Z]){1,50}$/,
+  messageRegex = /([a-zA-Z]){1,50}$/
+];
+
+// selecting form element
+var Contactform = document.querySelector('.Contact-form');
