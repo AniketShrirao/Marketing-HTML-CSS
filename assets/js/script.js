bc18667f6a1f0be13f2f56e1fc232160a58b5a6f
saniketprdxn <saniketprdxn@github.com>
2020-03-13 11:52:38 +0530

    js completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,107 +2,107 @@
  /*=================================
   Slider function starts here
 =================================*/
-// // Slider Container
-// var slider = document.querySelector(".slider");
-// // Control Buttons
-// var btns = document.querySelectorAll(".btn");
-// // Slides/Images
-// var slides = document.querySelectorAll(".slide figure");
-// // Carousel Dots
-// var dots = document.querySelectorAll('.dot');
+// Slider Container
+var slider = document.querySelector(".slider");
+// Control Buttons
+var btns = document.querySelectorAll(".btn");
+// Slides/Images
+var slides = document.querySelectorAll(".slide figure");
+// Carousel Dots
+var dots = document.querySelectorAll('.dot');
 
-// // carousel utility function
-// function SliderCarousel(slider,btns,slides,dots) {
-// 	var index = 1;
-// 	var dotIndex = 0;
-// 	var size = '100';
+// carousel utility function
+function SliderCarousel(slider,btns,slides,dots) {
+	var index = 1;
+	var dotIndex = 0;
+	var size = '100';
 
-// 	// to Animate the caousel
-// 	setInterval(ChangeDotOnBtn,4000);
+	// to Animate the caousel
+	setInterval(ChangeDotOnBtn,4000);
 
-// // To transition the slides
-// 	function slide() {
-// 		slider.classList.remove('transition-inactive');		
-// 		slider.classList.add('transition-active');
-// 		update();
-// 	}
+// To transition the slides
+	function slide() {
+		slider.classList.remove('transition-inactive');		
+		slider.classList.add('transition-active');
+		update();
+	}
 
-// 	// To translate the slides
-// 	function update() {
-// 		slider.style.transform = "translateX("+ (-size * index) +"%)";
-// 		// to make current Carousel dot active
-// 		dots.forEach(function(dot) {
-// 			dot.classList.remove('dot-active');
-// 		});
-// 		dots[dotIndex].classList.add('dot-active');
-// 	}
-// 	update();
+	// To translate the slides
+	function update() {
+		slider.style.transform = "translateX("+ (-size * index) +"%)";
+		// to make current Carousel dot active
+		dots.forEach(function(dot) {
+			dot.classList.remove('dot-active');
+		});
+		dots[dotIndex].classList.add('dot-active');
+	}
+	update();
 
-// 	// change the index of carousel dots based on current slide
-// 	function ChangeDotOnBtn() {
-// 		if(this.id === "prev") {
-// 			if(index <= 0) {
-// 				return;
-// 			}
-// 			index--;
-// 			if(dotIndex === 0){
-// 				dotIndex = 4;
-// 			}
-// 			else {
-// 				dotIndex--;
-// 			}
-// 		}
-// 		else {
-// 			if(index === slides.length-1 ) {
-// 				return;
-// 			}
-// 			index++;
-// 			if(dotIndex === 4) {
-// 				dotIndex = 0;
-// 			}
-// 			else {
-// 				dotIndex++;
-// 			}
-// 		}
-// 		slide();
-// 	}
+	// change the index of carousel dots based on current slide
+	function ChangeDotOnBtn() {
+		if(this.id === "prev") {
+			if(index <= 0) {
+				return;
+			}
+			index--;
+			if(dotIndex === 0){
+				dotIndex = 2;
+			}
+			else {
+				dotIndex--;
+			}
+		}
+		else {
+			if(index === slides.length-1 ) {
+				return;
+			}
+			index++;
+			if(dotIndex === 2) {
+				dotIndex = 0;
+			}
+			else {
+				dotIndex++;
+			}
+		}
+		slide();
+	}
 
-// 	// get the dots current value from data-attribute
-// 	function dotChange() {
-// 		let i = Number(this.getAttribute('dot-index'));
-// 		dotIndex = i;
-// 		index = i + 1;
-// 		slide();
-// 	}
+	// get the dots current value from data-attribute
+	function dotChange() {
+		let i = Number(this.getAttribute('dot-index'));
+		dotIndex = i;
+		index = i + 1;
+		slide();
+	}
 
-// 	// cloning of first and last slide to give slider effect at the end of every transition
-// 	slider.addEventListener('transitionend', function() {
-// 		if(slides[index].parentNode.id === "first") {
-// 			slider.classList.add('transition-inactive');		
-// 			slider.classList.remove('transition-active');
-// 			index = slides.length - 2;
-// 			slider.style.transform = "translateX("+ (-size * index) +"%)";
-// 		}
-// 		else if(slides[index].parentNode.id === "last") {
-// 			slider.classList.add('transition-inactive');		
-// 			slider.classList.remove('transition-active');
-// 			index = 1;
-// 			slider.style.transform = "translateX("+ (-size * index) +"%)";
-// 		}
-// 	})
-// 	// Events assigned to the Control buttons
-// 	btns.forEach( function(btn) {
-// 		btn.addEventListener('click', ChangeDotOnBtn);
-// 		clearInterval(ChangeDotOnBtn);
-// 	});
-// 	// Events assigned to the Carousel dots
-// 	dots.forEach(function(dot) {
-// 		dot.addEventListener('click', dotChange);
-// 	});
-// }
+	// cloning of first and last slide to give slider effect at the end of every transition
+	slider.addEventListener('transitionend', function() {
+		if(slides[index].parentNode.id === "first") {
+			slider.classList.add('transition-inactive');		
+			slider.classList.remove('transition-active');
+			index = slides.length - 2;
+			slider.style.transform = "translateX("+ (-size * index) +"%)";
+		}
+		else if(slides[index].parentNode.id === "last") {
+			slider.classList.add('transition-inactive');		
+			slider.classList.remove('transition-active');
+			index = 1;
+			slider.style.transform = "translateX("+ (-size * index) +"%)";
+		}
+	})
+	// Events assigned to the Control buttons
+	btns.forEach( function(btn) {
+		btn.addEventListener('click', ChangeDotOnBtn);
+		clearInterval(ChangeDotOnBtn);
+	});
+	// Events assigned to the Carousel dots
+	dots.forEach(function(dot) {
+		dot.addEventListener('click', dotChange);
+	});
+}
 
-// // carousel utility function called
-// SliderCarousel(slider,btns,slides,dots);
+// carousel utility function called
+SliderCarousel(slider,btns,slides,dots);
 
  /*=================================
   Slider function ends here
@@ -228,38 +228,53 @@ LightBox(html,ownersLightBox,Lightbox,contentInsideLightbox,close);
   owners-lightBox function ends here
 =================================*/
 
+
  /*=================================
-  ContactUs form Validation function start here
+  Modal form Validation function starts here
+=================================*/
+
+
+ /*=================================
+  Modal form Validation function ends here
+=================================*/
+
+
+ /*=================================
+ Validation function start here
 =================================*/
 
 // Selecting Every Input field
-var inputs = [
-  firstName = document.querySelector('#Contact-form-name'),
-  email = document.querySelector('#Contact-form-email'),
-  subject = document.querySelector('#Contact-form-subject'),
-  message = document.querySelector('#Contact-form-message'),
-];
-// Retrieving Values of every input field
-var values = {
-  fname: firstName.value,
-  emailId: email.value,
-  subject: subject.value,
-  message: subject.value
-};
+var inputs = Array.from(document.querySelectorAll('.Contact-form .form-group input'));
+// Selecting Every Input field of Modal
+var inputsModal = Array.from(document.querySelectorAll('.signup-modal .form-group input'));
 // Regex for every input fields
 var Regex = [
-  firstNameRegex = /([a-zA-Z]){1,20}$/,
+  firstNameRegex = /^([a-zA-Z]){0,20}$/,
   emailRegex = /^([0-9a-zA-Z\_\.\-]+)@([0-9a-zA-Z\_\.\-]+)\.([a-zA-Z]+)$/,
-  subjectRegex = /([a-zA-Z]){1,50}$/,
-  messageRegex = /([a-zA-Z]){1,50}$/
+  subjectRegex = /([a-zA-Z]){0,50}$/,
+  messageRegex = /([a-zA-Z]){0,50}$/
+];
+// Regex for every input fields in Modal
+var RegexModal = [
+  firstNameRegex = /^([a-zA-Z]){0,20}$/,
+  emailRegex = /^([0-9a-zA-Z\_\.\-]+)@([0-9a-zA-Z\_\.\-]+)\.([a-zA-Z]+)$/,
+  passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8})/,
+  confirmPasswordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8})/
 ];
 
 // selecting form element
 var Contactform = document.querySelector('.Contact-form');
+var signupform = document.querySelector('.signup-modal form');
 
 // Declaration of Reset Function make whole form reset
-function resetIt() {
-  inputs.forEach(function (input) {
+function resetIt(e) {
+  var currentInputs;
+  if(e.target.id == "submit") {
+    currentInputs = inputsModal;
+  } else {
+    currentInputs = inputs;
+  }
+  currentInputs.forEach(function (input) {
     var HelperClass = input.nextElementSibling.classList.contains("helperMessage");
     var successClass = input.nextElementSibling.classList.contains("success");
     var errorClass = input.nextElementSibling.classList.contains("error");
@@ -273,9 +288,16 @@ function resetIt() {
   });
 };
 
+// Send Button assigned a click Function
+var Button1 = document.querySelector('.send');
+Button1.addEventListener('click', function (e) {
+  e.preventDefault();
+  validateOnSubmit(e);
+});
+
 // Submit Button assigned a click Function
-var Button = document.querySelector('.send');
-Button.addEventListener('click', function (e) {
+var Button2 = document.querySelector('#submit');
+Button2.addEventListener('click', function (e) {
   e.preventDefault();
   validateOnSubmit(e);
 });
@@ -288,6 +310,26 @@ inputs.forEach(function (input) {
   });
 });
 
+// Keyup event on each input to check if it is valid
+inputsModal.forEach(function (input) {
+  var index = inputsModal.indexOf(input);
+  if (input.id !== "confirmPassword") {
+    input.addEventListener("keyup", function () {
+      validate(RegexModal[index], this);
+    });
+  } else {
+    //  special keyup event on confirm password to check if it matches with password field
+    input.addEventListener("keyup", function (e) {
+      if (inputsModal[2].value === inputsModal[3].value) {
+        validate(RegexModal[index], this);
+      } else {
+        input.nextElementSibling.classList = "helperMessage error";
+        errors(input, input.nextElementSibling);
+      }
+    });
+  }
+});
+
 // Defiination of Validate Function on Keyup
 function validate(RegularExpression, input) {
   if (input.value == "") {
@@ -297,14 +339,20 @@ function validate(RegularExpression, input) {
     input.nextElementSibling.innerText = "yay ! you are Correct!";
   } else {
     input.nextElementSibling.classList = "helperMessage error";
-    errors(input, input.nextElementSibling);
+      errors(input, input.nextElementSibling);
   }
 }
 
 // Defiination of Validate Function on submitButton
 function validateOnSubmit(e) {
+  var currentInputs;
+  if(e.target.id == "submit") {
+    currentInputs = inputsModal;
+  } else {
+    currentInputs = inputs;
+  }
   var allEmpty = false;
-  inputs.forEach(function (input) {
+  currentInputs.forEach(function (input) {
     if (input.value == "") {
       allEmpty = true;
     };
@@ -312,33 +360,43 @@ function validateOnSubmit(e) {
   // If every fields are empty show error
   if (allEmpty) {
     e.preventDefault();
-    inputs.forEach(function (input) {
+    currentInputs.forEach(function (input) {
       if (input.value == "") {
         input.nextElementSibling.classList.remove('none');
         input.nextElementSibling.classList.add('empty');
         errors(input, input.nextElementSibling);
       };
     });
-    var alert = document.querySelector('.Contact-form .customAlert');
-    var ok = document.querySelector('.Contact-form .confirmButton');
+    if(e.target.id== 'submit') {
+      alert("Please Fill the Form Correctly!");
+      return false;
+    }
+    var Customalert = document.querySelector('.contact .customAlert');
+    var ok = document.querySelector('.contact .confirmButton');
     var message = document.querySelector('.message');
     message.innerText = "Please Fill the Form Correctly!";
-    alert.classList.remove('fadeout');
-    alert.classList.add('morefadein');
+    Customalert.classList.remove('fadeout');
+    Customalert.classList.add('morefadein');
     ok.addEventListener('click', function () {
-      alert.classList.add('fadeout');
-      alert.classList.remove('morefadein');
-      alert.classList = "customAlert";
+      resetIt(e);
+      Customalert.classList.add('fadeout');
+      Customalert.classList.remove('morefadein');
+      Customalert.classList = "customAlert";
+
     });
     return false;
   }
+  var helperSpans;
+  if(e.target.id== 'submit') {
+    helperSpans = document.querySelectorAll('.signup-modal .helperMessage');
+  } else {
+    helperSpans = document.querySelectorAll('.Contact-form .helperMessage');
+  }
   // If every fields are invalid show errors message
-  var helperSpans = document.querySelectorAll('.Contact-form .helperMessage');
   var allCorrect = false;
 
   for(var index = 0; index< helperSpans.length; index++) {
     var SomeErrorify = false;
-    var AllErrorify = helperSpans[index].classList.contains('error');
     var AllCorrected = helperSpans[index].classList.contains('success');
       if(AllCorrected === true) {
         allCorrect = true;
@@ -350,20 +408,25 @@ function validateOnSubmit(e) {
         break;
       }
   }
-
-  // If every fields are correct alert success message
+  if(e.target.id== "submit") {
+    alert('Your Form Has Submitted Successfully!');
+    e.target.parentNode.parentNode.reset();
+    resetIt(e);
+    return false;
+  }
+  // If every fields are correct Customalert success message
   if (allCorrect == true) {
     Contactform.reset();
-    var alert = document.querySelector('.contact .customAlert');
+    var Customalert = document.querySelector('.contact .customAlert');
     var ok = document.querySelector('.customAlert .confirmButton');
     var message = document.querySelector('.customAlert .message');
     message.innerText = "Your Form Has Submitted Successfully!";
-    resetIt();
-    alert.classList.remove('fadeout');
-    alert.classList.add('fadein');
+    resetIt(e);
+    Customalert.classList.remove('fadeout');
+    Customalert.classList.add('fadein');
     ok.addEventListener('click', function () {
-      alert.classList.add('fadeout');
-      alert.classList.remove('fadein');
+      Customalert.classList.add('fadeout');
+      Customalert.classList.remove('fadein');
     });
   }
 }
@@ -381,21 +444,24 @@ function errors(input, span) {
     case "Contact-form-email":
       span.innerText = "Entered Email is Invalid.";
       break;
+    case "name":
+      span.innerText = "Must Contains Only Alphabets.";
+      break;
+    case "email":
+      span.innerText = "Entered Email is Invalid.";
+      break;
+    case "password":
+      span.innerText = "Must Contains One Alphabet(upperCase & LowerCase each) and a Number and Special Character.";
+      break;
+    case "confirmPassword":
+      span.innerText = "confirm password & password fields must match";
+      break;
     default:
       break;
   }
 }
  /*=================================
-  ContactUs form Validation function ends here
-=================================*/
-
- /*=================================
-  Modal form Validation function starts here
-=================================*/
-
-
- /*=================================
-  Modal form Validation function ends here
+ Validation function ends here
 =================================*/
 
  /*=================================
