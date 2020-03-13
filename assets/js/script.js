d304d4340eb632851f315e19af8d8de5165c83f4
saniketprdxn <saniketprdxn@github.com>
2020-03-13 17:22:33 +0530

    hamburger added

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,6 +109,25 @@ SliderCarousel(slider,btns,slides,dots);
 =================================*/
 
  /*=================================
+  Hamburger function ends here
+=================================*/
+  var hamburger = document.querySelector('.hamburger');
+  hamburger.addEventListener('click',function(){
+    var navigationUl = document.querySelector('nav ul');
+    if(navigationUl.classList.contains('navScale')) {
+      navigationUl.classList.remove('navScale');
+      hamburger.classList.remove('hamburger-active');
+    } else {
+      navigationUl.classList.add('navScale');
+      hamburger.classList.add('hamburger-active');
+    }
+  })
+
+ /*=================================
+  Hamburger function ends here
+=================================*/
+
+/*=================================
   Form Modal function starts here
 =================================*/
 // Get Elements Required for FormModal
